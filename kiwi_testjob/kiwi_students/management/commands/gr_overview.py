286d475698da764e10f75146b436c618905f9c82
andyrovno <andrew.rovno@gmail.com>
2012-09-18 08:50:14 +0300

    Update kiwi_testjob/kiwi_students/management/commands/gr_overview.py

--- kiwi_testjob/kiwi_students/management/commands/gr_overview.py
+++ kiwi_testjob/kiwi_students/management/commands/gr_overview.py
@@ -6,9 +6,7 @@ class Command( NoArgsCommand ):
      requires_model_validation = True
 
      def handle_noargs(self, **options):
-#          from django.db.models import get_app, get_model
           lines = []
-          #model = get_model(*( 'studbase', 'Groups'))
           groups = Groups.objects.select_related('starosta')
           for group in groups:
                lines.append("[%s]" % group.namegr)
